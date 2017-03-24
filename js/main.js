7d83ffd5e1dbf20ceb7a1a4ce8b9355d0663cb14
Husain <husainahmmed1b@gmail.com>
2017-03-24 07:15:10 +0600

    5 part update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,33 @@
 // Custom JS Script code here and jQuery plugins activation
 $(document).ready(function() {
-	$(".owl-carousel").owlCarousel({
-		items: 1,
+	$(".owl-carousel.slider-active").owlCarousel({
 		loop: true,
+		items: 1,
 		nav: true,
+		animateOut: 'zoomOut',
+    	animateIn: 'zoomIn',
+		smartSpeed: 1500,
+		autoplay: true,
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 3
+			},
+			1000: {
+				items: 1 
+			}
+		}
+	});
+	$(".owl-carousel.testimonial-carousel-active").owlCarousel({
+		loop: true,
+		items: 1,
+		nav: true,
+		animateOut: 'slideOutDown',
+    	animateIn: 'flipInX',
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
 		smartSpeed: 1500,
 		autoplay: true,
 		autoplayHoverPause: true,
